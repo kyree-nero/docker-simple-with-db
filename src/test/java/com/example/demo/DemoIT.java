2d289a947b6b82e305272d79a5daef528f40439b
userx <userx@userxs-mbp>
2020-09-28 22:51:48 -0400

    +junit 5

--- src/test/java/com/example/demo/DemoIT.java
+++ src/test/java/com/example/demo/DemoIT.java
@@ -1,17 +1,10 @@
 package com.example.demo;
 
 import org.hamcrest.Matchers;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.jdbc.Sql;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -20,22 +13,13 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment=SpringBootTest.WebEnvironment.MOCK)
-@ActiveProfiles("test")
-@TestPropertySource("/application-test.properties")
-@Sql({
-	"/META-INF/data/h2/drops.sql", 
-	"/META-INF/data/h2/tables.sql", 
-	"/META-INF/data/h2/inserts.sql"
-})
-@AutoConfigureMockMvc
-public class DemoIT {
+
+public class DemoIT extends AbstractIT{
 	@Autowired MockMvc mockMvc;
 	@Autowired WebApplicationContext webApplicationContext;
 	
 	
-	@Before public void before() {
+	@BeforeEach public void before() {
 		mockMvc = MockMvcBuilders
 			.webAppContextSetup(webApplicationContext)
 			.build();
